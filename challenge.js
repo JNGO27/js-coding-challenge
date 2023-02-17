5fa91fd5dca6b329549d2911829c03231d6a88dc
Joseph Ochoa <josephochoa91@gmail.com>
2023-02-17 11:35:58 -0800

    Add function that finds uniqueDates

---
+++ challenge.js
@@ -0,0 +1,43 @@
+const response = [
+  {
+    id: 1293487,
+    name: 'KCRW', // radio station callsign
+    tracks: [{ timestp: '2021-04-08', trackName: 'Peaches' }],
+  },
+  {
+    id: 12923,
+    name: 'KQED',
+    tracks: [
+      { timestp: '2021-04-09', trackName: 'Savage' },
+      { timestp: '2021-04-09', trackName: 'Savage (feat. Beyonce)' },
+      { timestp: '2021-04-08', trackName: 'Savage' },
+      { timestp: '2021-04-08', trackName: 'Savage' },
+      { timestp: '2021-04-08', trackName: 'Savage' },
+    ],
+  },
+  {
+    id: 4,
+    name: 'WNYC',
+    tracks: [
+      { timestp: '2021-04-09', trackName: 'Captain Hook' },
+      { timestp: '2021-04-08', trackName: 'Captain Hook' },
+      { timestp: '2021-04-07', trackName: 'Captain Hook' },
+    ],
+  },
+];
+
+function uniqueDates(apiResponse) {
+  const datesSet = new Set();
+
+  apiResponse.forEach(({ tracks }) =>
+    tracks.forEach(({ timestp }) => {
+      if (!datesSet.has(timestp)) {
+        datesSet.add(timestp);
+      }
+    })
+  );
+
+  return datesSet;
+}
+
+console.log(uniqueDates(response));
